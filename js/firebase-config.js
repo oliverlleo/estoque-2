7ccfe8631d980b9cfa1c1861baa88f61b8bcfd24
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2025-07-30 19:53:09 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,15 @@
 // Adicione aqui as suas configurações do Firebase
 // NOTA: Este é um exemplo e deve ser substituído pelas suas chaves reais.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC3DT4WwCJbeguyZ8myyZ3H0alTJUpn-bE",
+  authDomain: "estoque-d3354.firebaseapp.com",
+  databaseURL: "https://estoque-d3354-default-rtdb.firebaseio.com",
+  projectId: "estoque-d3354",
+  storageBucket: "estoque-d3354.firebasestorage.app",
+  messagingSenderId: "1051581921795",
+  appId: "1:1051581921795:web:5ff083ef333d35c890ec36",
+  measurementId: "G-5WQV7SHDZC"
 };
 
 // Importa as funções do Firebase SDK
